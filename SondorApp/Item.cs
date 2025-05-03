a17240e003deff54c56e6c144122cea7dafd9c9a
E1ecTro5 <abdulkerimovr7@gmail.com>
2025-05-03 10:40:34 +0400

    Forgot to add ctor for Item.cs

--- SondorApp/Item.cs
+++ SondorApp/Item.cs
@@ -8,6 +8,12 @@ namespace SondorApp
 {
     public class Item
     {
+        public Item(string name, int count)
+        {
+            Name = name;
+            Count = count;
+        }
+
         public string Name { get; set; }
         public int Count { get; set; }
     }
