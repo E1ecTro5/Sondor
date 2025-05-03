4f2c5cbf0cebdb99d9881380a49e65f5ed70ba92
E1ecTro5 <abdulkerimovr7@gmail.com>
2025-05-03 10:31:29 +0400

    Starting up.
    Should've been add the Model class first rather than this...

---
+++ SondorApp/DataManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SondorApp
+{
+    static class DataManager
+    {
+        //Засунул всё в .dat файл рядом с .exe'шником || Можно было бы и SQLite использовать, но для маленького проекта решил проигнорировать...
+        static string path = Directory.GetCurrentDirectory() + "\\main.dat";
+
+        public static string GetData()
+        {
+            return path;
+        }
+    }
+}
