ff8c219bb9cf307888f11bc8c03ca4be43822fb0
E1ecTro5 <abdulkerimovr7@gmail.com>
2025-05-03 11:41:43 +0400

    DataManager Additems chnaged completly.
    -Now it also works with lists.

--- SondorApp/DataManager.cs
+++ SondorApp/DataManager.cs
@@ -10,11 +10,46 @@ namespace SondorApp
     static class DataManager
     {
         //Засунул всё в .dat файл рядом с .exe'шником || Можно было бы и SQLite использовать, но для маленького проекта решил проигнорировать...
-        static string path = Directory.GetCurrentDirectory() + "\\main.dat";
+        private static string path = Directory.GetCurrentDirectory() + "\\main.dat";
+        private static List<Item> items;
+
+        public static void LoadItems()
+        {
+            using (StreamReader reader = new(path))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+
+                    string itemName = line.Split(";")[0];
+                    int itemCount = Convert.ToInt32(line.Split(";")[1]); // Исключения не обработал тут, при AddItem проверять буду наверное..
+
+                    items.Add(new Item(itemName, itemCount));
+                }
+            }
+        }
 
         public static void AddItem(Item item)
         {
-            using(StreamWriter writer = new(path, true))
+            //Negative value check
+            if(item.Count <= 0)
+            {
+                Console.WriteLine("Значение должно быть больше нуля!"); //Я потом более красиво это оформлю, пока что так для проверки оставим
+                return;
+            }
+
+            //Existency check
+            if(items.Select(x => x.Name).Contains(item.Name))
+            {
+                Item previous = items.Where(x => x.Name == item.Name).First();
+                Item change = new Item(item.Name, previous.Count + item.Count);
+                items[items.IndexOf(previous)] = change;
+
+                Console.WriteLine($"Добавлено {item.Count}шт к {item.Name} | Общее количество теперь: {change.Count}");
+            }
+
+            //Если новый
+            using (StreamWriter writer = new(path, true))
             {
                 writer.WriteLine($"{item.Name};{item.Count}"); //Здесь ; используется как separator
             }
--- SondorApp/Program.cs
+++ SondorApp/Program.cs
@@ -5,7 +5,9 @@
         static void Main(string[] args)
         {
             //DataManager.AddItem(new Item("First", 20));
-            //DataManager.AddItem(new Item("Second", 14));
+            ////DataManager.AddItem(new Item("Second", 14));
+            //DataManager.AddItem(new Item("Third", 104));
+            //DataManager.AddItem(new Item("Second", 104));
 
             DataManager.GetData();
         }
